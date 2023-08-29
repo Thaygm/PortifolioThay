512801fca5892bb57ed8aff4367497c747ea107a
Thay Mendes <thay.gm@live.com>
2023-08-29 15:01:25 -0300

    new

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,42 +1,51 @@
 export const Projetos = [
-{
-    nome: 'Clone Disney', img: 'https://raw.githubusercontent.com/Thaygm/clone_disneyplus/main/.github/disney%2B.png'
-    , github: 'https://github.com/Thaygm/clone_disneyplus'
-    , vercel: 'https://clone-disneyplus-3f4ampnpv-thaygm.vercel.app/?vercelToolbarCode=NaKeOA31BMJgqGp'
-},
-{
-    nome: 'Caveirinha', img: 'https://raw.githubusercontent.com/DenisDev3/caveirinha/main/src/assets/images/banner.jpg'
-    , github: 'https://github.com/DenisDev3/caveirinha'
-    , vercel: 'https://estofador.vercel.app/product/1'
-},
-{
-    nome: 'Portifólio', img: 'https://raw.githubusercontent.com/Thaygm/PortifolioThay/main/src/assets/Captura%20de%20tela%202023-07-13%20221652.png'
-    , github: 'https://github.com/Thaygm/PortifolioThay'
-    , vercel: 'https://estofador.vercel.app/product/1'
-},
-{
-    nome: 'Agenda de Contatos', img: 'https://raw.githubusercontent.com/Thaygm/Agenda_contatos/main/img/agenda.png'
-    , github: 'https://github.com/Thaygm/Agenda_contatos'
-    , vercel: 'https://agenda-contatos-seven.vercel.app/'
-},
-{
-    nome: 'Restaurant Layout', img: 'https://raw.githubusercontent.com/Thaygm/restaurante/main/images/restaurante.png'
-    , github: 'https://github.com/Thaygm/restaurante'
-    , vercel: 'https://restaurante-dun.vercel.app/'
-},
-{
-    nome: 'Atelier LB', img: 'https://raw.githubusercontent.com/Thaygm/Atelier3_site/main/images/atelier.png'
-    , github: 'https://github.com/Thaygm/Atelier3_site'
-    , vercel: 'https://atelier3-site.vercel.app/'
-},
-{
-    nome: 'Arch Site', img: 'https://raw.githubusercontent.com/Thaygm/Arch_site/main/IMAGENS/arch.png'
-    , github: 'https://github.com/Thaygm/Arch_site'
-    , vercel: 'https://arch-site-seven.vercel.app/'
-},
-{
-    nome: 'Clone ???', img: 'HTML DO PROJ'
-    , github: 'https://github.com/'
-    , vercel: 'https://agenda-'
-},
-]
+  {
+    nome: "Clone Disney",
+    img: "https://raw.githubusercontent.com/Thaygm/clone_disneyplus/main/.github/disney%2B.png",
+    github: "https://github.com/Thaygm/clone_disneyplus",
+    vercel:
+      "https://clone-disneyplus-3f4ampnpv-thaygm.vercel.app/?vercelToolbarCode=NaKeOA31BMJgqGp",
+  },
+  {
+    nome: "Caveirinha",
+    img: "https://raw.githubusercontent.com/DenisDev3/caveirinha/main/src/assets/images/banner.jpg",
+    github: "https://github.com/DenisDev3/caveirinha",
+    vercel: "https://estofador.vercel.app/product/1",
+  },
+  {
+    nome: "Portifólio",
+    img: "https://raw.githubusercontent.com/Thaygm/PortifolioThay/main/src/assets/Captura%20de%20tela%202023-07-13%20221652.png",
+    github: "https://github.com/Thaygm/PortifolioThay",
+    vercel: "https://estofador.vercel.app/product/1",
+  },
+  {
+    nome: "Agenda de Contatos",
+    img: "https://raw.githubusercontent.com/Thaygm/Agenda_contatos/main/img/agenda.png",
+    github: "https://github.com/Thaygm/Agenda_contatos",
+    vercel: "https://agenda-contatos-seven.vercel.app/",
+  },
+  {
+    nome: "Restaurant Layout",
+    img: "https://raw.githubusercontent.com/Thaygm/restaurante/main/images/restaurante.png",
+    github: "https://github.com/Thaygm/restaurante",
+    vercel: "https://restaurante-dun.vercel.app/",
+  },
+  {
+    nome: "Atelier LB",
+    img: "https://raw.githubusercontent.com/Thaygm/Atelier3_site/main/images/atelier.png",
+    github: "https://github.com/Thaygm/Atelier3_site",
+    vercel: "https://atelier3-site.vercel.app/",
+  },
+  {
+    nome: "Arch Site",
+    img: "https://raw.githubusercontent.com/Thaygm/Arch_site/main/IMAGENS/arch.png",
+    github: "https://github.com/Thaygm/Arch_site",
+    vercel: "https://arch-site-seven.vercel.app/",
+  },
+  {
+    nome: "Plin Project",
+    img: "../../assets/plinproject.png",
+    github: "https://github.com/Thaygm/plinProject",
+    vercel: "https://plin-project.vercel.app/",
+  },
+];
